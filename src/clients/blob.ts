b3e168e8896d56b983e2216672150df6961c113f
Dan Whitacre <whitacre.74@osu.edu>
2023-06-24 15:25:32 -0400

    some refactoring, mostly cleaning up tests

--- src/clients/blob.ts
+++ src/clients/blob.ts
@@ -1,6 +1,8 @@
-import { Server } from '@hapi/hapi'
-import { BlobClient, BlobServiceClient } from '@azure/storage-blob'
-import { getQueueName } from './queue'
+import { Request, ResponseObject, ResponseToolkit, Server } from '@hapi/hapi'
+import { BlobServiceClient } from '@azure/storage-blob'
+import { Message, getQueueName } from './queue'
+
+export type Body = Message & { isBodyBlob?: boolean }
 
 export default class Blob {
   readonly server: Server
@@ -17,35 +19,55 @@ export default class Blob {
     return getQueueName(pathname)
   }
 
-  getContainerClient(pathname: string) {
+  async getContainerClient(pathname: string) {
     const containerName = this.getContainerName(pathname)
-    return this.client.getContainerClient(containerName)
-  }
-
-  async getBlobClient(pathname: string, blobname: string) {
-    const containerClient = this.getContainerClient(pathname)
-    if (!(await containerClient.exists())) throw new Error(`container does not exist for ${pathname}`)
-    return containerClient.getBlobClient(blobname)
-  }
-
-  async getBlob(blobClient: BlobClient) {
-    if (!(await blobClient.exists())) return null
-    const download = await blobClient.download()
-    await blobClient.deleteIfExists()
-    return download.readableStreamBody
-  }
-
-  async createContainerClient(pathname: string) {
-    const containerName = this.getContainerName(pathname)
-
     try {
       await this.client.createContainer(containerName)
+      this.server.logger.warn('blob.getContainerClient: queue does not exist, created..')
     } catch {}
     return this.client.getContainerClient(containerName)
   }
 
-  async createBlobClient(pathname: string, blobname: string) {
-    const containerClient = await this.createContainerClient(pathname)
-    return containerClient.getBlobClient(blobname)
+  async getBlob(pathname: string, blobname: string) {
+    const containerClient = await this.getContainerClient(pathname)
+    const blobClient = containerClient.getBlobClient(blobname)
+
+    if (!(await blobClient.exists())) return null
+
+    const download = await blobClient.download()
+    await blobClient.deleteIfExists()
+
+    return download.readableStreamBody
+  }
+
+  async toResponse(stream: NodeJS.ReadableStream, request: Request, h: ResponseToolkit): Promise<ResponseObject> {
+    const json = (await new Promise((resolve, reject) => {
+      let chunks = ''
+      stream.on('data', (chunk) => (chunks += chunk))
+      stream.on('end', () => {
+        try {
+          resolve(JSON.parse(chunks))
+        } catch (err) {
+          reject(err)
+        }
+      })
+      stream.on('error', reject)
+    })) as Body
+    request.logger.debug({ json }, 'parsed json')
+
+    const response = h.response(json.body).code(json.status)
+    Object.entries(json.headers).forEach(([key, value]) => {
+      response.header(key, value)
+    })
+
+    return response
+  }
+
+  async createBlob(pathname: string, blobname: string, body: Body) {
+    const containerClient = await this.getContainerClient(pathname)
+    const blockBlobClient = containerClient.getBlockBlobClient(blobname)
+
+    const content = JSON.stringify(body)
+    return blockBlobClient.upload(content, content.length)
   }
 }
